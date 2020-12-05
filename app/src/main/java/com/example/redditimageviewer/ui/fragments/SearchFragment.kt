47fb476204d8739ed81bb0f99966967c080584ff
geoffmschaller <32310187+geoffmschaller@users.noreply.github.com>
2020-12-05 15:31:21 -0800

    Initial commit

---
+++ app/src/main/java/com/example/redditimageviewer/ui/fragments/SearchFragment.kt
@@ -0,0 +1,38 @@
+package com.example.redditimageviewer.ui.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.redditimageviewer.R
+import com.example.redditimageviewer.databinding.SearchFragmentBinding
+import com.example.redditimageviewer.viewModels.SearchViewModel
+import kotlinx.android.synthetic.main.search_fragment.*
+
+class SearchFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SearchFragment()
+    }
+
+    private lateinit var viewModel: SearchViewModel
+    private lateinit var binding: SearchFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.search_fragment, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/redditimageviewer/viewModels/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.redditimageviewer.viewModels
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
